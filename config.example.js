82bbe0ba4fa2cf8cbf21c9cd18e510dc0cfd971c
morrigan <belma.dzi@gmail.com>
2017-11-16 16:33:40 +0100

    Update config

--- config.example.js
+++ config.example.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'API',
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
-    contractAddress: '',
+    scriptHash: '3d428cf6137bb0e09b82125a27dfc61a29723394',
     RPCEndpoint: 'http://neo.marinpetrunic.com:30333/',
+    RESTEndpoint: 'http://marinpetrunic.com:5000',
     wif: '',
 };
